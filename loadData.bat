46a43142edd50ef31b914e054a2acc2255ae9198
Phil Barber <Phil.Barber@marklogic.com>
2016-09-12 18:45:04 -0400

    Load some sample data.

---
+++ loadData.bat
@@ -0,0 +1,1 @@
+mlcp import -host localhost -port 8012 -username admin -password admin -input_file_path workspaces\MDR\NetworkDisplayExperiment\family.xml -mode local -input_file_type rdf -output_uri_prefix /people/
